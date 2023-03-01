4f5fe33d37505b478a9dbc0c3bfbb2d8f1ea078d
AlyLoutfy <alyloutfy99@gmail.com>
2023-03-01 12:12:50 +0200

    Main skeleton setup

---
+++ script.js
@@ -0,0 +1,31 @@
+function add(x, y) {
+  return x + y;
+}
+
+function subtract(x, y) {
+  return x - y;
+}
+
+function multiply(x, y) {
+  return x * y;
+}
+
+function divide(x, y) {
+  return x / y;
+}
+
+function operate(op, x, y) {
+  if (op === "+") {
+    return add(x,y);
+  } else if (op === "-") {
+    return subtract(x,y);
+  } else if (op === "*") {
+    return multiply(x,y);
+  } else if (op === "/") {
+    return divide(x,y);
+  } else {
+    return "Operation not available";
+  }
+}
+
+console.log();
